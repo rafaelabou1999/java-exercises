23dc327262bb678e83c6874196a2a07c158e67bb
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2023-04-29 13:02:07 -0300

    Update GuessingGame.java

--- guessing-game/GuessingGame.java
+++ guessing-game/GuessingGame.java
@@ -1,3 +1,4 @@
+
 package guessing;
 
 import java.util.Scanner;
@@ -7,7 +8,7 @@ public class GuessingGame {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("GUESSING GAME");
-		System.out.println("There will be 3 rounds and you will only have 4 guesses for each");
+		System.out.println("There will be 2 rounds and you will only have 4 guesses for each");
 		System.out.println("Enter a number between 1 and 10:");
 		int yourGuess = sc.nextInt();
 		int computerGuess = (int) Math.floor(Math.random()* 10+ 1);
@@ -15,7 +16,7 @@ public class GuessingGame {
 		int scoreComputer = 0;
 		int scorePerson = 0;
 		
-	        System.out.println("ROUND 1");
+		int count = 0;
 		for (guess = 0; guess < 5; guess++) {
 			if (yourGuess > computerGuess) {
 				System.out.println("Too high. Try again.");
@@ -46,7 +47,7 @@ public class GuessingGame {
 		System.out.println();
 		System.out.println("Total guesses: " + guess);
 		System.out.println("Computer (" + scoreComputer + ") X You (" + scorePerson + ")");
-		System.out.println("ROUND 2");
+		
 		guess = 0;
 		for (guess = 0; guess < 5; guess++) {
 			if (yourGuess > computerGuess) {
@@ -74,47 +75,15 @@ public class GuessingGame {
 				
 			}
 		}
-		
+	
 		System.out.println("Total guesses: " + guess);
 		System.out.println("Computer (" + scoreComputer + ") X You (" + scorePerson + ")");
-		
-		System.out.println("ROUND 3");
-		
-		guess = 0;
-		for (guess = 0; guess < 5; guess++) {
-			if (yourGuess > computerGuess) {
-				System.out.println("Too high. Try again.");
-				System.out.println("Enter a number between 1 and 10:");
-				yourGuess = sc.nextInt();
-				guess++;
-				if (guess == 5) {
-					System.out.println("YOU LOST ROUND 2");
-					scoreComputer += 1;
-				}
-			} else if (yourGuess < computerGuess){
-				System.out.println("Too low. Try again.");
-				System.out.println("Enter a number between 1 and 10:");
-				yourGuess = sc.nextInt();
-				guess++;
-				if (guess == 5) {
-					System.out.println("YOU LOST ROUND 2");
-					scoreComputer +=1;
-				}
-			} else {
-				System.out.println("YOU WON ROUND 2");
-				scorePerson += 1;
-				guess = 5;
-				
-			}
-		}
-		
-		System.out.println("Computer (" + scoreComputer + ") X You (" + scorePerson + ")");
-		
+	
 		if (scoreComputer > scorePerson) {
 			System.out.println("THE COMPUTER WON!");
-		} else {
+		} else if (scoreComputer < scorePerson){
 			System.out.println("YOU WON!");
-		}
+		} 
 		sc.close();
 	}
 }
